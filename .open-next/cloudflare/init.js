20ace36cd8f88a2b39cbe556ed0794ce0db56bd6
Chan Shu You <you.sebastian.1.618@Chans-MacBook-Pro.local>
2025-11-22 12:48:10 -0800

    removed direct url

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1763843026141,
+    __BUILD_TIMESTAMP_MS__: 1763843568574,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
